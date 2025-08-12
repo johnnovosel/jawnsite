6a8f757ba739b3e67523527377e1b1297dd98782
John Novosel <johnnovosel96@gmail.com>
2025-08-12 10:37:36 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,84 @@
+import { useState } from 'react'
+import './App.css'
+import BOOKS from './books.json'
+
+function SearchBar({ searchTerm, searchTermChange }) {
+
+  return (
+    <form className="searchbar">
+      <input
+        type="text"
+        placeholder="Search books..."
+        value={searchTerm}
+        onChange={(e) => searchTermChange(e.target.value)} />
+    </form>
+  )
+
+}
+
+function BookRow({ book }) {
+  return (
+    <tr>
+      <td>{book.title}</td>
+      <td>{book.author}</td>
+    </tr>
+  )
+}
+
+function BookTable({ books, searchTerm }) {
+  const rows = [];
+
+  books.forEach((book) => {
+    if (book.title.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1 ||
+        book.author.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1) {
+      rows.push(
+        <BookRow
+          book={book}
+        />
+      );
+    }
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Author</th>
+        </tr>
+      </thead>
+      <tbody>
+        {rows.length > 0 ? (
+          rows
+        ) : (
+          <tr>
+            <td colSpan={2} style={{ textAlign: 'center', fontStyle: 'italic', color: '#888' }}>
+              No books found
+            </td>
+          </tr>
+        )}
+      </tbody>
+    </table>
+  );
+}
+
+function FilterableBookList({ books }) {
+
+  const [searchTerm, setSearchTerm] = useState('')
+
+  return (
+    <div>
+      <SearchBar
+        searchTerm={searchTerm}
+        searchTermChange={setSearchTerm} />
+      <BookTable
+        books={books}
+        searchTerm={searchTerm} />
+    </div>
+  )
+}
+
+export default function App() {
+
+  return <FilterableBookList books={BOOKS} />
+}
