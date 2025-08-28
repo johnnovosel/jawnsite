1b3f05acf42ab81c2c996c46dc5c3ac0f730fff1
John Novosel <johnnovosel96@gmail.com>
2025-08-28 00:06:20 -0500

    ADDED BUTTON

--- app/page.js
+++ app/page.js
@@ -1,7 +1,22 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+
+import styles from './page.module.css';
+
 export default function Page() {
+
+  const router = useRouter()
+
   return (
     <div>
       <h1>Welcome to BookApp</h1>
+
+
+      <button type="button" className={styles.ctabutton} onClick={() => router.push('/Book')}>
+        CLICK HERE FOR BOOK LIST
+      </button>
+
     </div>
   );
 }
